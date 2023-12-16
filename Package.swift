418bb75d3557d906de06672fa601bbfb8ad61fb7
Abdullah Sönmez <99615897+abdullahsonmez49@users.noreply.github.com>
2023-12-16 21:57:21 +0300

    Update Package.swift
    version update

--- Package.swift
+++ Package.swift
@@ -3,17 +3,14 @@ import PackageDescription
 let package = Package(
     name: "IubendaMobileSDK",
     platforms: [
-        .iOS(.v10) // or the appropriate platform
+        .iOS(.v12) // or the appropriate platform
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "IubendaMobileSDK",
             targets: ["IubendaMobileSDK"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "IubendaMobileSDK",
             path: "https://libraries.iubenda.com/ios/IubendaMobileSDK/iubenda.xcframework-2.7.1.zip"
